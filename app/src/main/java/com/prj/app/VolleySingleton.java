8698178f172c2cc2d8469443f0b5106788c5b084
Danilo Del Busso <danilo.delbusso1@gmail.com>
2021-02-25 14:23:27 +0100

    Add Volley singleton for API requests

---
+++ app/src/main/java/com/prj/app/VolleySingleton.java
@@ -0,0 +1,27 @@
+package com.prj.app;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class VolleySingleton {
+    public static final String API_URL = "http://192.168.1.10:4683/api/v1/";
+    private static VolleySingleton instance;
+    private RequestQueue requestQueue;
+
+    private VolleySingleton(Context context) {
+        requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+    }
+
+    public static VolleySingleton getInstance(Context context) {
+        if (instance == null) {
+            instance = new VolleySingleton(context);
+        }
+        return  instance;
+    }
+
+    public RequestQueue getRequestQueue(){
+        return requestQueue;
+    }
+}
