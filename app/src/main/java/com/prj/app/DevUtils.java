28456e2a39669058763f606dac95969ef15e95cd
Danilo Del Busso <danilo.delbusso1@gmail.com>
2021-03-01 22:53:13 +0100

    Add BSSID matching logic

--- app/src/main/java/com/prj/app/DevUtils.java
+++ app/src/main/java/com/prj/app/DevUtils.java
@@ -9,16 +9,9 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.Request;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.List;
 import java.util.Objects;
 
-@SuppressLint({"UseSwitchCompatOrMaterialCode", "SetTextI18n"})
+@SuppressLint({"UseSwitchCompatOrMaterialCode", "SetTextI18n", "SimpleDateFormat"})
 public class DevUtils extends AppCompatActivity {
     private DatabaseManager databaseManager;
 
@@ -33,60 +26,28 @@ public class DevUtils extends AppCompatActivity {
     }
 
     public void toggleLocationSwitch(View view) {
-        Switch switchView = (Switch) findViewById(R.id.locationSwitch);
+        Switch switchView = findViewById(R.id.locationSwitch);
         boolean newValue = !databaseManager.canSaveHotspotLocation();
         databaseManager.setSaveHotspotLocation(newValue);
         switchView.setChecked(newValue);
     }
 
     private void initialiseSettings() {
-        Switch switchView = (Switch) findViewById(R.id.locationSwitch);
+        Switch switchView = findViewById(R.id.locationSwitch);
         switchView.setChecked(databaseManager.canSaveHotspotLocation());
     }
 
     public void getMatchingBSSIDs(View view) {
-        DatabaseManager databaseManager = new DatabaseManager(this);
-        List<String> results = databaseManager.getScanBSSIDs();
-
-        JSONObject jsonBody = new JSONObject();
-
-        try {
-            jsonBody.put("BSSIDs", new JSONArray(results));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
-        resultTextView.setText("Getting matching BSSIDs");
-
-        String URL = VolleySingleton.API_URL + "scans/get/matchBSSID";
-        sendMatchingBSSIDsPOST(URL, jsonBody);
+        MatchMaker matchMaker = new MatchMaker(databaseManager,
+                findViewById(R.id.resultTextView),
+                this.getApplicationContext());
+        matchMaker.getMatchingBSSIDs();
     }
 
-    private void sendMatchingBSSIDsPOST(String URL, JSONObject jsonBody) {
-        TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
-        try {
-            long startTime = System.nanoTime();
-            CustomJsonArrayRequest customJsonArrayRequest = new CustomJsonArrayRequest(
-                    Request.Method.POST,
-                    URL,
-                    jsonBody,
-                    response -> resultTextView.setText(
-                            "Cross referenced 5,000,000 scans\n" +
-                                    "Found " + response.length() + " results\n" +
-                                    "Time: " + ((System.nanoTime() - startTime) / 1000000) + "ms\n\n"),
-                    Throwable::printStackTrace);
-
-            VolleySingleton.getInstance(this).getRequestQueue().add(customJsonArrayRequest);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     public void uploadScans(View view) {
         Log.d("debug", "Uploading scans");
-        TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+        TextView resultTextView = findViewById(R.id.resultTextView);
         resultTextView.setText("Uploading scans");
     }
 
