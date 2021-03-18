7c3053fb9307d8ae5b9a451f45fb1421972449ed
Danilo Del Busso <danilo.delbusso1@gmail.com>
2021-03-18 14:17:48 +0100

    Add comment to getLocationAverage method

--- app/src/main/java/com/prj/app/Util.java
+++ app/src/main/java/com/prj/app/Util.java
@@ -7,6 +7,12 @@ import java.math.RoundingMode;
 import java.util.List;
 
 public class Util {
+
+    /** Extrapolate Latitude and Longitude of the average between two poins that lieon a great-circle. This method uses the Haversine formula for the great-circle distance between two points.
+     * As taken from https://stackoverflow.com/questions/6671183/calculate-the-center-point-of-multiple-latitude-longitude-coordinate-pairs?lq=1
+     * @param locations List of LatLng object corresponding to the locations to average
+     * @return a LatLng object representing the average point of all input LatLng objects
+     */
     public static LatLng getLocationAverage(List<LatLng> locations) {
         if (locations.size() == 1) {
             return locations.get(0);
